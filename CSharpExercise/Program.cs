9f48276b21a1c5e2aba6ecb7c257a4266415b5dc
Zelong Yu <zelong.yu@outlook.com>
2019-02-19 15:28:55 -0500

    Quadratic Equation method added

--- CSharpExercise/Program.cs
+++ CSharpExercise/Program.cs
@@ -22,6 +22,8 @@ namespace CSharpExercise
             Console.WriteLine("Circumference of Circle is {0}", circumferenceOfCircle(radius));
             Console.WriteLine("Volume of Hemisphere is {0}", volumeOfHemisphere(radius));
             Console.WriteLine("Area of Triangle is {0}", areaOfTriangle(3,4,5));
+            var solution = quadraticEqn(1, 2, 1);
+            Console.WriteLine("Solutions are {0}", solution.Item1, solution.Item2);
         }
 
         public static double areaOfCircle(double r)
@@ -47,6 +49,23 @@ namespace CSharpExercise
             double p = (a + b + c) / 2.0;
             double Area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
             return Area;
+        }
+
+        public static Tuple<double,double> quadraticEqn(int a, int b, int c)
+        {
+            double Delta = b * b - 4 * a * c;
+            if (Delta >= 0)
+            {
+                double x1 = (-b + Math.Sqrt(Delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(Delta)) / (2 * a);
+                var tuple = new Tuple<double, double>(x1, x2);
+                return tuple;
+            }
+            else
+            {
+                Console.WriteLine("No Real Solution.");
+                return new Tuple<double, double>(0.0, 0.0);
+            }
         }
     }
 }
