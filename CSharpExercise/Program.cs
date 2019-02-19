a3252708afd961952d426a7eb46f839a1e918d06
Zelong Yu <zelong.yu@outlook.com>
2019-02-19 14:27:14 -0500

    add Circumference method

--- CSharpExercise/Program.cs
+++ CSharpExercise/Program.cs
@@ -19,6 +19,7 @@ namespace CSharpExercise
             radius = Convert.ToDouble(userInput);
             Console.WriteLine("You entered {0}", radius);
             Console.WriteLine("Area of Circle is {0}", areaOfCircle(radius));
+            Console.WriteLine("Circumference of Circle is {0}", circumferenceOfCircle(radius));
         }
 
         public static double areaOfCircle(double r)
@@ -26,6 +27,12 @@ namespace CSharpExercise
             double Area = Math.PI * Math.Pow(r, 2);
             return Area;
         }
-        
+
+        public static double circumferenceOfCircle(double r)
+        {
+            double Cir = 2* Math.PI * r;
+            return Cir;
+        }
+
     }
 }
