d8054fa3738001f309ab138b3d6225e4e72cce70
Zelong Yu <zelong.yu@outlook.com>
2019-02-19 13:41:53 -0500

    Add project files.

---
+++ CSharpExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            double radius;
+
+            Console.Write("Enter radius：");
+            userInput = Console.ReadLine();
+            /* Converts to double type */
+            radius = Convert.ToDouble(userInput);
+            Console.WriteLine("You entered {0}", radius);
+        }
+    }
+}
