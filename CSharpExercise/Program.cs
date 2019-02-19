61cd7bcf074209f06b777e3ae057038dd37c0ea3
Zelong Yu <zelong.yu@outlook.com>
2019-02-19 14:53:25 -0500

    Area of Triangle method added

--- CSharpExercise/Program.cs
+++ CSharpExercise/Program.cs
@@ -21,6 +21,7 @@ namespace CSharpExercise
             Console.WriteLine("Area of Circle is {0}", areaOfCircle(radius));
             Console.WriteLine("Circumference of Circle is {0}", circumferenceOfCircle(radius));
             Console.WriteLine("Volume of Hemisphere is {0}", volumeOfHemisphere(radius));
+            Console.WriteLine("Area of Triangle is {0}", areaOfTriangle(3,4,5));
         }
 
         public static double areaOfCircle(double r)
@@ -37,8 +38,15 @@ namespace CSharpExercise
 
         public static double volumeOfHemisphere(double r)
         {
-            double Vol = ((4.0 / 3.0) * Math.PI * Math.Pow(r,3)) / 2.0;
+            double Vol = 4.0 / 3 * Math.PI * Math.Pow(r,3) / 2;//use 4.0 so that 4/3 is not 1
             return Vol;
+        }
+
+        public static double areaOfTriangle(int a, int b, int c)
+        {
+            double p = (a + b + c) / 2.0;
+            double Area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return Area;
         }
     }
 }
